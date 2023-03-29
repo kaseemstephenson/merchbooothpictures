ce0c18e0ab1756f99808c7695f12d3c92308166d
kaseemstephenson <kaseemstephenson.pal@gmail.com>
2023-03-28 22:23:57 -0400

    test

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+const path = require('path');
+
+var port = process.env.PORT || 4200;
 var app = express();
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
-});
+const dir = path.join(__dirname, '/public');
+
+app.use(express.static(__dirname + '/public'));
+
+//app.get('/', function (req, res) {
+ //res.send(JSON.stringify({ Hello: 'World'}));
+//});
 app.listen(port, function () {
  console.log('Example app listening on port !');
 });
